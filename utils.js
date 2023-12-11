3f784b00f4665301ba01382282b2ca7328bef930
nattadetspite <ntd.ctnts@gmail.com>
2023-12-11 09:38:28 +0700

    :sparkles: readData utils

---
+++ utils.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+
+const readData = (input) => {
+    const data = fs.readFileSync(input, { encoding: "utf8", flag: "r" }); // exampleInput.txt and input.txt
+    const dataArray = data.split("\n");
+    console.log(dataArray);
+    return dataArray;
+}
+
+module.exports = readData;
