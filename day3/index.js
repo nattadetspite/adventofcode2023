d7e66e4286a455dc863be7215d6b0085858f5f45
nattadetspite <ntd.ctnts@gmail.com>
2023-12-17 21:41:52 +0700

    wip: day3

--- day3/index.js
+++ day3/index.js
@@ -1,3 +1,23 @@
 const readData = require("../utils");
 
-const dataArray = readData('exampleInput.txt');
+const dataArray = readData("exampleInput.txt");
+
+/**
+ * find number
+ * find lenght number
+ * check adjacent around number is there any symbpl
+ * if any symbol add it in sum var.
+ */
+
+function isNumber (c) {
+    if (c >= '0' && c <= '9') return true;
+}
+
+for (let i = 0; i < dataArray.length; i++) {
+  let startNum = true;
+  let stopNum = false;
+
+for (let j = 0; j < dataArray[i].length; j++) {
+    
+  }
+}
--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const readData = (input) => {
     const data = fs.readFileSync(input, { encoding: "utf8", flag: "r" }); // exampleInput.txt and input.txt
-    const dataArray = data.split("\n");
+    const dataArray = data.replace(/\r/g, '').split("\n");
     console.log(dataArray);
     return dataArray;
 }
